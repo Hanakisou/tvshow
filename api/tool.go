593b5493c6b89c4673cd2f3fe5c01ce56588831c
YanG-1989 <109458285+YanG-1989@users.noreply.github.com>
2023-06-02 04:04:05 +0800

    Update tool.go

--- api/tool.go
+++ api/tool.go
@@ -2,12 +2,28 @@
 package api
 
 import (
+	"bytes"
+	"crypto/md5"
+	"encoding/base64"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io"
+	"math/rand"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+	 js "github.com/dop251/goja"
+	"github.com/etherlabsio/go-m3u8/m3u8"
+	"github.com/tidwall/gjson"
         "github.com/YanG-1989/list/blob/main/golang/douyu.go"
 	"github.com/YanG-1989/list/blob/main/golang/bilibili.go"
 	"github.com/YanG-1989/list/blob/main/golang/douyin.go"
 	"github.com/YanG-1989/list/blob/main/golang/huya.go"
 	"github.com/YanG-1989/list/blob/main/golang/youtube.go"
-	"net/http"
 )
 func Tool(w http.ResponseWriter, r *http.Request)  {
     // todo
