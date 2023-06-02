2a9a910a65b1c47dab36b8f7c36bbe8fdb979fd3
YanG-1989 <109458285+YanG-1989@users.noreply.github.com>
2023-06-02 19:29:23 +0800

    Update tool.go

--- api/tool.go
+++ api/tool.go
@@ -1,11 +1,14 @@
-
 package api
 
 import (
-	"net/http"
-	"github.com/YanG-1989/list/golang"
+   "net/http"
+   "github.com/YanG-1989/list/golang"
 )
 
-func Tool(w http.ResponseWriter, r *http.Request)  {
-    // todo
+func ToolGoHandler(w http.ResponseWriter, r *http.Request) {
+   // 处理 HTTP 请求的代码逻辑
+   // 例如，你可以使用 golang 包中的函数来处理请求
+   result := golang.SomeFunction(r)
+   // 然后返回处理结果
+   w.Write([]byte(result))
 }
